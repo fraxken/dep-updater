3197aecc510050b8e26888b470ddfb1f7a4862bb
Captainfive <vinou_montes@hotmail.fr>
2019-07-30 04:41:48 +0200

    fix(index): crash when all dep are up to date

--- bin/index.js
+++ bin/index.js
@@ -49,131 +49,139 @@ async function main() {
 
     console.log(`\n${gray().bold(" > npm outdated --json")}`);
     const { stdout } = spawnSync(`npm${EXEC_SUFFIX ? ".cmd" : ""}`, ["outdated", "--json"], SPAWN_OPTIONS);
-    const outdated = parseOutDatedDependencies(stdout);
 
-    // Define list of packages to update!
-    const packageToUpdate = [];
-    for (const pkg of outdated) {
-        if (pkg.current === pkg.latest) {
-            continue;
-        }
+    try {
+        const outdated = parseOutDatedDependencies(stdout);
 
-        const updateTo = pkg.wanted === pkg.current ? pkg.latest : pkg.wanted;
-        console.log(`\n${green().bold(pkg.name)} (${yellow().bold(pkg.current)} -> ${cyan().bold(updateTo)})`);
-        const { update } = await inquirer.prompt([questions.update_package]);
-        if (!update) {
-            continue;
-        }
 
-        pkg.kind = findPkgKind(localPackage, pkg);
-        pkg.updateTo = updateTo;
-
-        if (pkg.wanted !== pkg.latest && pkg.current !== pkg.wanted) {
-            const { release } = await inquirer.prompt([{
-                type: "list",
-                name: "release",
-                message: "which release do you want ?",
-                choices: [
-                    { name: `wanted (${yellow().bold(pkg.wanted)})`, value: pkg.wanted },
-                    { name: `latest (${yellow().bold(pkg.latest)})`, value: pkg.latest }
-                ],
-                default: 0
-            }]);
-
-            pkg.updateTo = release;
-        }
-
-        packageToUpdate.push(pkg);
-    }
-
-    // Exit if there is no package to update
-    if (packageToUpdate.length === 0) {
-        console.log(white().bold(`\nNo package to update.. ${red("exiting process")}`));
-        process.exit(0);
-    }
-
-    // Configuration
-    console.log(`\n${gray().bold(" > Configuration")}\n`);
-    const { runTest, gitCommit } = await inquirer.prompt([
-        questions.run_test,
-        questions.git_commit
-    ]);
-
-    // Verify test and git on the local root/system
-    console.log("");
-    let stopScript = false;
-    let gitUsername;
-    let gitEmail;
-
-    if (gitCommit) {
-        const { stdout: username } = spawnSync("git", ["config", "--global", "user.name"], SPAWN_OPTIONS);
-        gitUsername = username.toString().replace(/(\r\n|\n|\r)/gm, "");
-
-        const { stdout: useremail } = spawnSync("git", ["config", "--global", "user.email"], SPAWN_OPTIONS);
-        gitEmail = useremail.toString().replace(/(\r\n|\n|\r)/gm, "");
-    }
-
-    if (runTest) {
-        const scripts = localPackage.scripts || {};
-        if (Reflect.has(scripts, "test")) {
-            console.log("✔️ npm test script must exist");
-        }
-        else {
-            console.log("⛔️ Unable to found test script in local package.json");
-            stopScript = true;
-        }
-    }
-    if (stopScript) {
-        console.log("");
-        process.exit(0);
-    }
-
-    console.log(`\n${gray(" > Everything is okay ... Running update in one second.")}\n`);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // Run updates!
-    for (const pkg of packageToUpdate) {
-        console.log(`\nupdating ${bold(green(pkg.name))} (${yellow(pkg.current)} -> ${cyan(pkg.updateTo)})`);
-        const { status, remove } = update(pkg);
-        if (status !== 0) {
-            console.log(red(`\n > Failed to update ${pkg.name} package!`));
-            if (remove) {
-                console.log(" > package has been removed, rollback installation");
-                rollback(pkg, false);
+        // Define list of packages to update!
+        const packageToUpdate = [];
+        for (const pkg of outdated) {
+            if (pkg.current === pkg.latest) {
+                continue;
             }
-            continue;
+
+            const updateTo = pkg.wanted === pkg.current ? pkg.latest : pkg.wanted;
+            console.log(`\n${green().bold(pkg.name)} (${yellow().bold(pkg.current)} -> ${cyan().bold(updateTo)})`);
+            const { update } = await inquirer.prompt([questions.update_package]);
+            if (!update) {
+                continue;
+            }
+
+            pkg.kind = findPkgKind(localPackage, pkg);
+            pkg.updateTo = updateTo;
+
+            if (pkg.wanted !== pkg.latest && pkg.current !== pkg.wanted) {
+                const { release } = await inquirer.prompt([{
+                    type: "list",
+                    name: "release",
+                    message: "which release do you want ?",
+                    choices: [
+                        { name: `wanted (${yellow().bold(pkg.wanted)})`, value: pkg.wanted },
+                        { name: `latest (${yellow().bold(pkg.latest)})`, value: pkg.latest }
+                    ],
+                    default: 0
+                }]);
+
+                pkg.updateTo = release;
+            }
+
+            packageToUpdate.push(pkg);
+        }
+
+        // Exit if there is no package to update
+        if (packageToUpdate.length === 0) {
+            console.log(white().bold(`\nNo package to update.. ${red("exiting process")}`));
+            process.exit(0);
+        }
+
+        // Configuration
+        console.log(`\n${gray().bold(" > Configuration")}\n`);
+        const { runTest, gitCommit } = await inquirer.prompt([
+            questions.run_test,
+            questions.git_commit
+        ]);
+
+        // Verify test and git on the local root/system
+        console.log("");
+        let stopScript = false;
+        let gitUsername;
+        let gitEmail;
+
+        if (gitCommit) {
+            const { stdout: username } = spawnSync("git", ["config", "--global", "user.name"], SPAWN_OPTIONS);
+            gitUsername = username.toString().replace(/(\r\n|\n|\r)/gm, "");
+
+            const { stdout: useremail } = spawnSync("git", ["config", "--global", "user.email"], SPAWN_OPTIONS);
+            gitEmail = useremail.toString().replace(/(\r\n|\n|\r)/gm, "");
         }
 
         if (runTest) {
-            console.log(" > npm test");
-            try {
-                const { signal, status } = spawnSync(`npm${EXEC_SUFFIX ? ".cmd" : ""}`,
-                    ["test"], { ...SPAWN_OPTIONS, stdio: "inherit" });
-                strictEqual(signal, null);
-                strictEqual(status, 0);
+            const scripts = localPackage.scripts || {};
+            if (Reflect.has(scripts, "test")) {
+                console.log("✔️ npm test script must exist");
             }
-            catch (error) {
-                console.log(red("An Error occured while executing tests!"));
-                console.log("Rollback to previous version!");
-                rollback(pkg);
+            else {
+                console.log("⛔️ Unable to found test script in local package.json");
+                stopScript = true;
+            }
+        }
+        if (stopScript) {
+            console.log("");
+            process.exit(0);
+        }
 
+        console.log(`\n${gray(" > Everything is okay ... Running update in one second.")}\n`);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        // Run updates!
+        for (const pkg of packageToUpdate) {
+            console.log(`\nupdating ${bold(green(pkg.name))} (${yellow(pkg.current)} -> ${cyan(pkg.updateTo)})`);
+            const { status, remove } = update(pkg);
+            if (status !== 0) {
+                console.log(red(`\n > Failed to update ${pkg.name} package!`));
+                if (remove) {
+                    console.log(" > package has been removed, rollback installation");
+                    rollback(pkg, false);
+                }
                 continue;
             }
-        }
 
-        if (gitCommit) {
-            const name = pkg.name.length > 40 ? `${pkg.name.substr(0, 37)}...` : pkg.name;
-            const commitMsg = gitTemplate({ name, from: pkg.current, to: pkg.updateTo });
-            console.log(` > git commit -m ${yellow(commitMsg)}`);
+            if (runTest) {
+                console.log(" > npm test");
+                try {
+                    const { signal, status } = spawnSync(`npm${EXEC_SUFFIX ? ".cmd" : ""}`,
+                        ["test"], { ...SPAWN_OPTIONS, stdio: "inherit" });
+                    strictEqual(signal, null);
+                    strictEqual(status, 0);
+                }
+                catch (error) {
+                    console.log(red("An Error occured while executing tests!"));
+                    console.log("Rollback to previous version!");
+                    rollback(pkg);
 
-            await git.add({ dir: CWD, filepath: "package.json" });
-            if (havePackageLock) {
-                await git.add({ dir: CWD, filepath: "package-lock.json" });
+                    continue;
+                }
             }
-            await git.commit({ dir: CWD, message: commitMsg, author: { email: gitEmail, name: gitUsername } });
-        }
-    }
 
-    console.log("\nAll packages updated !\n");
+            if (gitCommit) {
+                const name = pkg.name.length > 40 ? `${pkg.name.substr(0, 37)}...` : pkg.name;
+                const commitMsg = gitTemplate({ name, from: pkg.current, to: pkg.updateTo });
+                console.log(` > git commit -m ${yellow(commitMsg)}`);
+
+                await git.add({ dir: CWD, filepath: "package.json" });
+                if (havePackageLock) {
+                    await git.add({ dir: CWD, filepath: "package-lock.json" });
+                }
+                await git.commit({ dir: CWD, message: commitMsg, author: { email: gitEmail, name: gitUsername } });
+            }
+        }
+
+        console.log("\nAll packages updated !\n");
+    }
+    catch (err) {
+        if (err.name === "SyntaxError")
+        console.log("All dependencies are up-to-date");
+    }
 }
 main().catch(console.error);
